474512a57a36d5c75ddcde6de07d4eb04fe216a5
Shapke01 <stanislaw.hapke@gmail.com>
2023-09-06 21:18:55 +0200

    download form gdrive

--- src/data_preparation/preparation_utils.py
+++ src/data_preparation/preparation_utils.py
@@ -2,18 +2,24 @@ import os
 import zipfile
 from zipfile import ZipFile
 from tqdm import tqdm
+import gdown
 import pandas as pd
 
-def prepareDataFrame(csv_paths: list[str]):
+def prepareDataFrame(csv_paths: list[str], images_path: str):
     for csv_path in csv_paths:
         if not os.path.isfile(csv_path):
             raise FileNotFoundError(f"Provided file: {csv_path} does not exist.")
+    if not os.path.isdir(images_path):
+        raise FileNotFoundError(f"Provided directory: {images_path} does not exist.")
     
     # mapping path to dataframe with full paths to images
-    def mapPath2DataFrame(path):
-        dataf = pd.read_csv(path)
-        folder_path = os.path.dirname(path)
-        dataf['fullpath'] = folder_path + '/' + dataf['filename']
+    def mapPath2DataFrame(csv_path):
+        dataf = pd.read_csv(csv_path)
+        fullpaths = images_path + '/' + dataf['filename']
+        for path in fullpaths:
+            if not os.path.isfile(path):
+                raise FileNotFoundError(f"The image: {path}, from dataframe: {csv_path} does not exist.")
+        dataf['fullpath'] = fullpaths
 
         return dataf
 
@@ -30,7 +36,15 @@ def zip2folder(src: str, dest: str):
 
     with ZipFile(src, 'r') as zip_ref:
         for file in tqdm(zip_ref.infolist(), desc='Extracting '):
-            try:
-                zip_ref.extract(file, dest)
-            except zipfile.error as e:
-                pass
+            fname = os.path.basename(file.filename)
+            if fname:
+                file.filename = fname
+                try:
+                    zip_ref.extract(file, dest)
+                except zipfile.error as e:
+                    pass
+
+def downloadGDriveFile(id: str, dest_file_path: str):
+    prefix = 'https://drive.google.com/uc?/export=download&id='
+    url = prefix + id
+    gdown.download(url = url, output=dest_file_path)
